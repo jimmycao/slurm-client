3d47807d569e9714b2f4d00eb29c29f02acd8951
jimmycao <jimmy.cao@emc.com>
2013-01-28 11:33:21 +0800

    modify client code

--- src/slurm_client.c
+++ src/slurm_client.c
@@ -19,16 +19,22 @@
 
 int main(void)
 {
-		char *query_total = "get total node and slot no";
-		char *query_available = "get available node and slot no";
+#if 1
+		char *query_total = "get total nodes and slots";
+		operation(query_total);
+
+		char *query_available = "get available nodes and slots";
+		operation(query_available);
+#endif
+
 #if 0
 		char *allocate_str = "allocate jobid=100 return=all timeout=10:app=0 np=5 N=2 node_list=vm2,vm3 flag=mandatory:app=1 N=2";
 		operation(allocate_str);
 #endif
 
 
-#if 1
-		char *deallocate = "deallocate slurm_jobid=744 job_return_code=0:slurm_jobid=745 job_return_code=-1";
+#if 0
+		char *deallocate = "deallocate slurm_jobid=749 job_return_code=0:slurm_jobid=750 job_return_code=-1";
 //		char *deallocate = "deallocate slurm_jobid=732 job_return_code=-1";
         operation(deallocate);
 #endif
